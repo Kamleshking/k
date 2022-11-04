7ee3dcdcad626d793e4ece4537553d0596a89f00
Kamleshking <101655383+Kamleshking@users.noreply.github.com>
2022-11-04 18:51:48 +0500

    Update unbuntu.sh

--- unbuntu.sh
+++ unbuntu.sh
@@ -3,17 +3,3 @@ apt upgrade -y
 apt install wget
 
 echo "deb https://termux.mentality.rip/termux-main stable main" > $PREFIX/etc/apt/sources.list && cat $PREFIX/etc/apt/sources.list && pkg update -y && pkg install wget curl proot tar -y && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu20/ubuntu20.sh -O ubuntu20.sh && chmod +x ubuntu20.sh && bash ubuntu20.sh
-
-apt update
-apt upgrade
-apt install wget curl git npm nano nodejs openjdk-8-jdk openjdk-8-jre -y
-source <(curl -fsSL https://raw.githubusercontent.com/efxtv/npm/main/apktool/apktool-kali-ubuntu.sh)
- git clone https://github.com/efxtv/L3MON
- cd L3MON
- npm install pm2 -g
-npm install
-npm audit fix
-npm audit
-pm2 start index.js
-pm2 startup
-pm2 stop index.js
