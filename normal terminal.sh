47c6858427a729e899c7ea458960fe8633f01f26
Kamleshking <101655383+Kamleshking@users.noreply.github.com>
2022-11-04 18:14:44 +0500

    Add files via upload

---
+++ normal terminal.sh
@@ -0,0 +1,9 @@
+pkg update && pkg upgrade
+source <(curl -fsSL https://raw.githubusercontent.com/efxtv/npm/main/apktool/apktool-termux.sh)
+
+source <(curl -fsSL https://raw.githubusercontent.com/efxtv/npm/main/L3mon-no-java8.sh)
+
+curl -L -o $PWD/emsf https://github.com/efxtv/EMSF/blob/main/termux/emsf?raw=true -s;chmod +x emsf;mv emsf ../usr/bin/
+
+emsf fix 
+
